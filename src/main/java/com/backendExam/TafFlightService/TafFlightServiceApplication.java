33f286b8a294eba8a8b8d72a712000fb32e63934
Shruthi R <shruthir@tekarch.com>
2025-01-23 00:10:39 -0800

    working code

--- src/main/java/com/backendExam/TafFlightService/TafFlightServiceApplication.java
+++ src/main/java/com/backendExam/TafFlightService/TafFlightServiceApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class TafFlightServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TafFlightServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TafFlightServiceApplication.class, args);
+    }
 
 }
